9a5b011b28ce7a932df50e130fed2b9abbf44d34
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-11 16:58:19 -0400

    starting project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+
+import { errorHandling } from "./middlewares/error-handling"
+import z from "zod"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello, world2")
+})
+
+app.use(errorHandling)
+
+export { app }
