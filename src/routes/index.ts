1b70e120fa35f1a6eb5a82a4a383060bc62ed14f
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-14 10:13:49 -0400

    Create ensure-authenticated middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 
 import { usersRoutes } from "./users-routes";
 import { sessionRoutes } from "./sessions-routes";
+import { refundsRoutes } from "./refunds-routes";
+
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router()
 
 //Rotas publicas
-
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionRoutes)
+
+//rotas privadas
+routes.use(ensureAuthenticated)
+routes.use("/refunds", refundsRoutes)
 
 export { routes }
