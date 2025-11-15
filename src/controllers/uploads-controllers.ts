00b44831b22118c9a86f12c24f006bf277963e04
Marcos <marcos.2h.oshiro@gmail.com>
2025-11-15 15:50:04 -0400

    validating file

--- src/controllers/uploads-controllers.ts
+++ src/controllers/uploads-controllers.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from "express"
+import z from "zod"
+
+import uploadConfig from "@/configs/upload"
 
 class UploadsController {
     async create(request: Request, response: Response) {
-        response.json({ file: request.file })
+        try {
+            const fileSchema = z.object({
+                filename: z.string().min(1, { message: "Arquivo obrigatório" }),
+                mimetype: z.string().refine((type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type), {
+                    message: "Tipo de arquivo inválido. Formatos permitidos: " + uploadConfig.ACCEPTED_IMAGE_TYPES
+                }),
+                size: z.number().positive().refine((size) => uploadConfig.MAX_FILE_SIZE >= size, {
+                    message: `O tamanho máximo permitido é de 3 MB`
+                }),
+                path: z.string(),
+            })
+
+            const { file } = fileSchema.parse(request.file)
+
+        } catch (error) {
+            throw error
+        }
     }
 }
 
